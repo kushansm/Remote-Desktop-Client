d64b74819a7a81a2f72e096883271c1daa43584f
Vimukthi Dilshan <desilvavimukthi911@gmail.com>
2025-01-10 07:48:39 +0530

    configure setup

--- client-app/src/main/java/lk/ijse/dep13/client/controller/MainSceneController.java
+++ client-app/src/main/java/lk/ijse/dep13/client/controller/MainSceneController.java
@@ -1,54 +1,41 @@
 package lk.ijse.dep13.client.controller;
 
-import javafx.scene.layout.AnchorPane;
 import javafx.concurrent.Task;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 
-
-import java.awt.Point;
-import java.io.*;
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
 import java.net.Socket;
 
 public class MainSceneController {
-    public ImageView imgScreen;
+    public ImageView imgCamera;
     public AnchorPane root;
-    private Socket socket;
 
-    public void initialize() throws Exception {
-        imgScreen.fitWidthProperty().bind(root.widthProperty());
-        imgScreen.fitHeightProperty().bind(root.heightProperty());
-
-        socket = new Socket("127.0.0.1", 9090);
-        OutputStream os = socket.getOutputStream();
-        BufferedOutputStream bos = new BufferedOutputStream(os);
-        ObjectOutputStream oos = new ObjectOutputStream(bos);
-
-        imgScreen.setOnMouseMoved(mouseEvent -> {
-            try {
-                oos.writeObject(new Point((int) mouseEvent.getX(), (int) mouseEvent.getY()));
-                oos.flush();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
+    public void initialize(){
+        imgCamera.fitWidthProperty().bind(root.widthProperty());
+        imgCamera.fitHeightProperty().bind(root.heightProperty());
 
         Task<Image> task = new Task<>() {
+
             @Override
             protected Image call() throws Exception {
+                Socket socket = new Socket("127.0.0.1", 9090);
                 InputStream is = socket.getInputStream();
                 BufferedInputStream bis = new BufferedInputStream(is);
                 ObjectInputStream ois = new ObjectInputStream(bis);
-                while (true) {
+
+                while (true){
                     byte[] image = (byte[]) ois.readObject();
-                    ByteArrayInputStream bais = new ByteArrayInputStream(image);
-                    Image screen = new Image(bais);
-                    updateValue(screen);
+                    updateValue(new Image(new ByteArrayInputStream(image)));
                 }
             }
         };
 
-        imgScreen.imageProperty().bind(task.valueProperty());
+        imgCamera.imageProperty().bind(task.valueProperty());
         new Thread(task).start();
     }
 }
--- server-app/src/main/java/lk/ijse/dep13/server/SeverAppInitializer.java
+++ server-app/src/main/java/lk/ijse/dep13/server/SeverAppInitializer.java
@@ -1,57 +1,42 @@
 package lk.ijse.dep13.server;
 
+//import com.github.sarxos.webcam.Webcam;
+
 import javax.imageio.ImageIO;
-import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-
 public class SeverAppInitializer {
 
-    public static void main(String[] args) throws Exception {
 
-        ServerSocket serverSocket = new ServerSocket(9090);
-        System.out.println("Server started on port 9090");
-        while (true) {
-            System.out.println("Waiting for connection...");
-            Socket localSocket = serverSocket.accept();
-            System.out.println("Accepted connection from " + localSocket.getRemoteSocketAddress());
-            new Thread(()->{
-                try {
-                    OutputStream os = localSocket.getOutputStream();
-                    BufferedOutputStream bos = new BufferedOutputStream(os);
-                    ObjectOutputStream oos = new ObjectOutputStream(bos);
+        public static void main(String[] args) throws IOException {
+            Webcam webcam = Webcam.getDefault();
+            webcam.open();
+            ServerSocket serverSocket = new ServerSocket(9090);
+            System.out.println("Server started on port 9090");
+            while (true) {
+                System.out.println("Waiting for connection...");
+                Socket localSocket = serverSocket.accept();
+                System.out.println("Accepted connection from " + localSocket.getRemoteSocketAddress());
+                new Thread(()->{
+                    try {
+                        OutputStream os = localSocket.getOutputStream();
+                        BufferedOutputStream bos = new BufferedOutputStream(os);
+                        ObjectOutputStream oos = new ObjectOutputStream(bos);
 
-                    while (true) {
-                        Robot robot = new Robot();
-                        BufferedImage screen = robot
-                                .createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
-                        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                        ImageIO.write(screen, "jpeg", baos);
-                        oos.writeObject(baos.toByteArray());
-                        oos.flush();
-                        Thread.sleep(1000 / 27);
+                        while (true) {
+                            BufferedImage image = webcam.getImage();
+                            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                            ImageIO.write(image, "jpg", baos);
+                            oos.writeObject(baos.toByteArray());
+                            oos.flush();
+                            Thread.sleep(1000 / 27);
+                        }
+                    }catch (Exception e){
+                        e.printStackTrace();
                     }
-                }catch (Exception e){
-                    e.printStackTrace();
-                }
-            }).start();
-            new Thread(()->{
-                try {
-                    InputStream is = localSocket.getInputStream();
-                    BufferedInputStream bis = new BufferedInputStream(is);
-                    ObjectInputStream ois = new ObjectInputStream(bis);
-
-                    Robot robot = new Robot();
-                    while (true){
-                        Point coordinates = (Point) ois.readObject();
-                        robot.mouseMove(coordinates.x, coordinates.y);
-                    }
-                }catch (Exception e){
-                    e.printStackTrace();
-                }
-            }).start();
+                }).start();
+            }
         }
     }
-}
